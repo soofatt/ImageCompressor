e567694fbdc2225825d6a7e3809efd83f703e971
Chiang Choon Yong <yong931231@hotmail.com>
2014-10-17 17:14:41 +0800

    Implement IDCT to inverse transform 1 column of 2D array. Modify 2D IDCT without transposing method. Documentation

--- src/IDCT.h
+++ src/IDCT.h
@@ -10,7 +10,9 @@ void dumpMatrix(int size,float matrix[][size]);
 float cos_IDCT(int numerator, int denominator);
 float int_Divider(int numerator, int denominator);
 void round_float(float array[], int noOfELement);
-void oneD_IDCT(float transVal[], int noOfElement);
+void oneD_IDCT_row(float transVal[], int noOfElement);
+void oneD_IDCT_column(int size, float transVal[][size], int column);
+void twoD_IDCT_with_transpose(int size,float transVal[][size]);
 void twoD_IDCT(int size,float transVal[][size]);
 void transpose_2D(int size, float matrix[][size]);
 #endif // IDCT_H
