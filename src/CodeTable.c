10ed15ba428f2d7e84a8a91666509ac00e1efdca
soofatt <soofatt93@gmail.com>
2014-12-12 13:11:45 +0800

    Add test for Code Table

--- src/CodeTable.c
+++ src/CodeTable.c
@@ -1,30 +1,36 @@
 #include "CodeTable.h"
 #include <stdio.h>
+#include <assert.h>
 
 void createTable(CodeTable *codeTable, RunSizeCode runSizeCode[], int index){
-  // int slots = 0, currentIndex, shift, i;
+  int slots = 0, currentIndex, shift, i;
   
-  // currentIndex = index;
+  currentIndex = index;
   
-  // for(i = 0; i < 16, currentIndex < sizeof(runSizeCode); ){
-    // if(runSizeCode[currentIndex].codeLength > 4 && runSizeCode[currentIndex].codeLength <= 8)
-      // shift = runSizeCode[currentIndex].codeLength - 4;
-    // else if(runSizeCode[currentIndex].codeLength > 8 && runSizeCode[currentIndex].codeLength <= 12)
-      // shift = runSizeCode[currentIndex].codeLength - 8;
-    // else if(runSizeCode[currentIndex].codeLength > 12 && runSizeCode[currentIndex].codeLength <= 16)
-      // shift = runSizeCode[currentIndex].codeLength - 12;
-    // else
-      // shift = runSizeCode[currentIndex].codeLength;
+  for(i = 0; i < 16; ){
+    if(runSizeCode[currentIndex].codeLength > 4 && runSizeCode[currentIndex].codeLength <= 8)
+      shift = runSizeCode[currentIndex].codeLength - 4;
+    else if(runSizeCode[currentIndex].codeLength > 8 && runSizeCode[currentIndex].codeLength <= 12)
+      shift = runSizeCode[currentIndex].codeLength - 8;
+    else if(runSizeCode[currentIndex].codeLength > 12 && runSizeCode[currentIndex].codeLength <= 16)
+      shift = runSizeCode[currentIndex].codeLength - 12;
+    else
+      shift = runSizeCode[currentIndex].codeLength;
+
+    slots = 1 << (4 - shift);
+    assert(currentIndex < 8);
     
-    // slots = 1 << (4 - shift);
+    if(runSizeCode[currentIndex].type == RUNSIZECODE){
+      while(slots != 0){
+        // assert(codeTable->table[i] != NULL); // Check if table is not null
+        codeTable->table[i] = (CodeTable *)(&runSizeCode[currentIndex]);
+        i++;
+        slots--;
+      }
+    }
+    else
+      i++;
     
-    // if(runSizeCode[currentIndex].type == RUNSIZECODE){
-      // while(slots != 0){
-        // (unsigned int)codeTable->table[i] = runSizeCode[currentIndex].runSize;
-        // i++;
-      // }
-    // }
-    
-    // currentIndex++;
-  // }
+    currentIndex++;
+  }
 }
--- test/test_CodeTable.c
+++ test/test_CodeTable.c
@@ -7,12 +7,30 @@ void tearDown(void){}
 
 void test_createTable_given_4_codes_up_to_codeLength_4(void){
 	CodeTable *codeTable;
-  RunSizeCode runSizeCode[4] = {{.type = RUNSIZECODE, .runSize = 0x01, .codeLength = 2},
-                                {.type = RUNSIZECODE, .runSize = 0x02, .codeLength = 2},
+  RunSizeCode runSizeCode[8] = {{.type = RUNSIZECODE, .runSize = 0x00, .codeLength = 2},
+                                {.type = RUNSIZECODE, .runSize = 0x01, .codeLength = 3},
+                                {.type = RUNSIZECODE, .runSize = 0x02, .codeLength = 3},
                                 {.type = RUNSIZECODE, .runSize = 0x03, .codeLength = 3},
-                                {.type = RUNSIZECODE, .runSize = 0x00, .codeLength = 4}};
-                                
+                                {.type = RUNSIZECODE, .runSize = 0x04, .codeLength = 3},
+                                {.type = RUNSIZECODE, .runSize = 0x05, .codeLength = 3},
+                                {.type = RUNSIZECODE, .runSize = 0x06, .codeLength = 4},
+                                {.type = CODETABLE, .runSize = 0x07, .codeLength = 5}};
+                         
   createTable(codeTable, runSizeCode, 0);
   
-  TEST_ASSERT_EQUAL(runSizeCode[4].runSize, codeTable->table[0]);
+  TEST_ASSERT_EQUAL(&runSizeCode[0], codeTable->table[0]);
+  TEST_ASSERT_EQUAL(&runSizeCode[0], codeTable->table[1]);
+  TEST_ASSERT_EQUAL(&runSizeCode[0], codeTable->table[2]);
+  TEST_ASSERT_EQUAL(&runSizeCode[0], codeTable->table[3]);
+  TEST_ASSERT_EQUAL(&runSizeCode[1], codeTable->table[4]);
+  TEST_ASSERT_EQUAL(&runSizeCode[1], codeTable->table[5]);
+  TEST_ASSERT_EQUAL(&runSizeCode[2], codeTable->table[6]);
+  TEST_ASSERT_EQUAL(&runSizeCode[2], codeTable->table[7]);
+  TEST_ASSERT_EQUAL(&runSizeCode[3], codeTable->table[8]);
+  TEST_ASSERT_EQUAL(&runSizeCode[3], codeTable->table[9]);
+  TEST_ASSERT_EQUAL(&runSizeCode[4], codeTable->table[10]);
+  TEST_ASSERT_EQUAL(&runSizeCode[4], codeTable->table[11]);
+  TEST_ASSERT_EQUAL(&runSizeCode[5], codeTable->table[12]);
+  TEST_ASSERT_EQUAL(&runSizeCode[5], codeTable->table[13]);
+  TEST_ASSERT_EQUAL(&runSizeCode[6], codeTable->table[14]);
 }
